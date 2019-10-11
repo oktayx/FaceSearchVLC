d4ec93bbd73354f0cb3b46e5d799ada9f63ac344
Oktay Işıktekiner <okitayf1@gmail.com>
2019-10-11 13:33:00 +0300

    Add project files.

---
+++ ConsoleToast/Program.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.WindowsAPICodePack.Shell.PropertySystem;
+using MS.WindowsAPICodePack.Internal;
+using System;
+using System.Diagnostics;
+using System.IO;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+/// <summary>
+/// Modified version of ConsoleToast in Github
+/// Used to Windows 10 display notification from console app
+/// </summary>
+namespace ConsoleToast
+{
+    public class Program
+    {
+        public string ImagePath { get; set; }
+        public string NotificationText { get; set; }
+        private string NotificationTemplate { get; set; }
+
+        public Program()
+        {
+            //Required to show notification
+            ShortCutCreator.TryCreateShortcut("FaceSearch.App", "FaceSearch");
+
+            //Load notification template from file
+            NotificationTemplate = File.ReadAllText("toast1.xml");
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+
+        public void ShowImageToastM(string appId, string title, string message, string image)
+        {
+            string modXml = NotificationTemplate.Replace("%EXEPATH%", Environment.CurrentDirectory);
+            modXml = modXml.Replace("%TEXT%", message);
+            modXml = modXml.Replace("%HEADER%", title);
+            modXml = modXml.Replace("%IMAGE%", image);
+
+
+            XmlDocument toastXml = new XmlDocument();
+
+            toastXml.LoadXml(modXml);
+
+
+            // Create the toast and attach event listeners
+            ToastNotification toast = new ToastNotification(toastXml);
+
+            ToastEvents events = new ToastEvents() { Title = title };
+
+            toast.Activated += events.ToastActivated;
+            toast.Dismissed += events.ToastDismissed;
+            toast.Failed += events.ToastFailed;
+
+            // Show the toast. Be sure to specify the AppUserModelId
+            // on your application's shortcut!
+            ToastNotificationManager.CreateToastNotifier(appId).Show(toast);
+        }
+
+        class ToastEvents
+        {
+            public string Title { get; set; }
+
+            internal void ToastActivated(ToastNotification sender, object e)
+            {
+                //Replace space for url
+                Title = Title.Replace(" ", "%20");
+                //Show title in chrome. Customize for other browser or url
+                Process.Start("chrome.exe", $"https://www.imdb.com/find?ref_=nv_sr_fn&q={Title}&s=all");
+
+                Console.WriteLine("User activated the toast");
+            }
+
+            internal void ToastDismissed(ToastNotification sender, ToastDismissedEventArgs e)
+            {
+                String outputText = "";
+                switch (e.Reason)
+                {
+                    case ToastDismissalReason.ApplicationHidden:
+                        outputText = "The app hid the toast using ToastNotifier.Hide";
+                        break;
+                    case ToastDismissalReason.UserCanceled:
+                        outputText = "The user dismissed the toast";
+                        break;
+                    case ToastDismissalReason.TimedOut:
+                        outputText = "The toast has timed out";
+                        break;
+                }
+
+                Console.WriteLine(outputText);
+            }
+
+            internal void ToastFailed(ToastNotification sender, ToastFailedEventArgs e)
+            {
+                Console.WriteLine("The toast encountered an error.");
+            }
+        }
+
+        static class ShortCutCreator
+        {
+            // In order to display toasts, a desktop application must have
+            // a shortcut on the Start menu.
+            // Also, an AppUserModelID must be set on that shortcut.
+            // The shortcut should be created as part of the installer.
+            // The following code shows how to create
+            // a shortcut and assign an AppUserModelID using Windows APIs.
+            // You must download and include the Windows API Code Pack
+            // for Microsoft .NET Framework for this code to function
+
+            internal static bool TryCreateShortcut(string appId, string appName)
+            {
+                String shortcutPath = Environment.GetFolderPath(
+                    Environment.SpecialFolder.ApplicationData) +
+                    "\\Microsoft\\Windows\\Start Menu\\Programs\\" + appName + ".lnk";
+                if (!File.Exists(shortcutPath))
+                {
+                    InstallShortcut(appId, shortcutPath);
+                    return true;
+                }
+                return false;
+            }
+
+            static void InstallShortcut(string appId, string shortcutPath)
+            {
+                // Find the path to the current executable
+                String exePath = Process.GetCurrentProcess().MainModule.FileName;
+                IShellLinkW newShortcut = (IShellLinkW)new CShellLink();
+
+                // Create a shortcut to the exe
+                VerifySucceeded(newShortcut.SetPath(exePath));
+                VerifySucceeded(newShortcut.SetArguments(""));
+
+                // Open the shortcut property store, set the AppUserModelId property
+                IPropertyStore newShortcutProperties = (IPropertyStore)newShortcut;
+
+                using (PropVariant applicationId = new PropVariant(appId))
+                {
+                    VerifySucceeded(newShortcutProperties.SetValue(
+                        SystemProperties.System.AppUserModel.ID, applicationId));
+                    VerifySucceeded(newShortcutProperties.Commit());
+                }
+
+                // Commit the shortcut to disk
+                IPersistFile newShortcutSave = (IPersistFile)newShortcut;
+
+                VerifySucceeded(newShortcutSave.Save(shortcutPath, true));
+            }
+
+            static void VerifySucceeded(UInt32 hresult)
+            {
+                if (hresult <= 1)
+                    return;
+
+                throw new Exception("Failed with HRESULT: " + hresult.ToString("X"));
+            }
+        }
+    }
+}
---
+++ nfwFolderCheck/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nfwFolderCheck
+{
+    class Program
+    {
+        static FileSystemWatcher watcher;
+        static string SnapshotFolder;
+        //Required to send captured photo & get face recognition result
+        static string apiAddress = "http://127.0.0.1:5555/api/face/SearchFace?treshold=45";
+        static ConsoleToast.Program toast;
+
+        static void Main(string[] args)
+        {
+            //Customize snapshot folder to watch. Default is VLC snapshot folder
+            SnapshotFolder = GetVLCCaptureFolder();
+            if(SnapshotFolder == "")
+            {
+                Console.WriteLine("Watch folder not found");
+                Environment.Exit(0);
+            }
+
+            //Initiate notification module
+            toast = new ConsoleToast.Program();
+
+            //Start to watch folder to get captured images 
+            WatchFolder();
+
+            new System.Threading.AutoResetEvent(false).WaitOne();
+
+        }
+
+        public static void WatchFolder()
+        {
+            watcher = new FileSystemWatcher();
+            watcher.Path = SnapshotFolder;
+            watcher.NotifyFilter = NotifyFilters.Attributes |
+                                    NotifyFilters.CreationTime |
+                                    NotifyFilters.FileName |
+                                    NotifyFilters.LastAccess |
+                                    NotifyFilters.LastWrite |
+                                    NotifyFilters.Size |
+                                    NotifyFilters.Security;
+            watcher.Filter = "*.*";
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            watcher.EnableRaisingEvents = true;
+        }
+
+        public static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            string json = UploadFile(e.FullPath);
+            Console.WriteLine(json);
+
+            //My json format: {"AD":"Türkan Soray","LISTE":"Sinema","SKOR":0.475338876}
+            //Customize return data properties from face service
+            dynamic data = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+            string title = data.AD==null ? "Person not found!" : data.AD;
+            string message = data.LISTE==null ? "" : data.LISTE;
+
+            toast.ShowImageToastM(
+                    "FaceSearch.App",
+                    title,
+                    message,
+                    e.FullPath);
+        }
+
+        public static string UploadFile(string filePath)
+        {
+            FileInfo file = new FileInfo(filePath);
+
+            while (IsFileLocked(file))
+                System.Threading.Thread.Sleep(500);
+
+            using (var wc = new WebClient())
+            {
+                var taskResult = wc.UploadFileTaskAsync(apiAddress, filePath);
+                taskResult.Wait();
+
+                return System.Text.Encoding.UTF8.GetString(taskResult.Result);
+            }
+        }
+
+        protected static bool IsFileLocked(FileInfo file)
+        {
+            FileStream stream = null;
+
+            try
+            {
+                stream = file.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            }
+            catch (IOException)
+            {
+                //the file is unavailable because it is:
+                //still being written to
+                //or being processed by another thread
+                //or does not exist (has already been processed)
+                return true;
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+            }
+
+            //file is not locked
+            return false;
+        }
+
+        public static string GetVLCCaptureFolder()
+        {
+            string appDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string vlcConfigFile = Path.Combine(appDataFolder, "vlc", "vlcrc");
+            string folder = "";
+            try
+            {
+                folder = File.ReadAllLines(vlcConfigFile).Where(l => l.StartsWith("snapshot-path")).Select(f => f.Split('=').ElementAt(1)).Single();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("VLC config not found");
+            }
+
+            return folder;
+        }
+    }
+
+}
